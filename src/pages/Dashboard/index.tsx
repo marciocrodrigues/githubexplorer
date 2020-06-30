c84491d55aa537619251063eef78f2336b949725
Marcio Costa Rodrigues <marciodeath@hotmail.com>
2020-06-30 12:01:27 -0300

    integrando com api do github com axios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,57 +1,68 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { AxiosTransformer, AxiosResponse } from 'axios';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
+import Respository from '../Repository';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [newRepo, setNewRepo] = useState('');
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input type="text" placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          value={newRepo}
+          placeholder="Digite o nome do repositório"
+          onChange={e => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="/">
-          <img
-            src="https://avatars2.githubusercontent.com/u/40520700?s=460&v=4"
-            alt=""
-          />
-          <div>
-            <strong>marciocrodrigues</strong>
-            <p>Test Descrição</p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="/">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.full_name}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="/">
-          <img
-            src="https://avatars2.githubusercontent.com/u/40520700?s=460&v=4"
-            alt=""
-          />
-          <div>
-            <strong>marciocrodrigues</strong>
-            <p>Test Descrição</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="/">
-          <img
-            src="https://avatars2.githubusercontent.com/u/40520700?s=460&v=4"
-            alt=""
-          />
-          <div>
-            <strong>marciocrodrigues</strong>
-            <p>Test Descrição</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
